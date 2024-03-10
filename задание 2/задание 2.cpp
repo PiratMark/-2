3b849725d6c51be0fc08817bbb5e9c214a7bfe79
Компьютер <@DESKTOP-JR5U4KS>
2024-03-10 15:50:28 +0500

    Добавьте файлы проекта.

---
+++ задание 2/задание 2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+	  int x, y, z;
+	  std::cout << "x y: "; 
+	  std::cin >> x >> y;
+	  z = x; 
+	  while
+		  (--y)z *= x;
+	  std::cout << z; 
+
+
+
+
+}
+
